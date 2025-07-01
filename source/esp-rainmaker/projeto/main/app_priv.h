d0dc245e0dc57a5712142205d7a668f2ee088b2e
enzoaccioly1 <146199749+enzoaccioly1@users.noreply.github.com>
2025-07-01 13:43:17 -0300

    Add files via upload

---
+++ source/esp-rainmaker/projeto/main/app_priv.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+#define REPORTING_PERIOD 10 // segundos
+#define DEFAULT_POWER false
+
+extern esp_rmaker_device_t *temp_sensor_device;
+extern esp_rmaker_device_t *humidity_sensor_device;
+extern esp_rmaker_device_t *pressure_sensor_device;
+extern esp_rmaker_device_t *relay_device;
+extern esp_rmaker_device_t *LDR_device;
+
+void app_driver_init(void);
+int app_driver_set_state(bool state);
+float app_get_current_temperature();
+float app_get_current_humidity();
+float app_get_current_pressure();
+int app_get_current_luminosity();
+bool app_driver_get_state();
